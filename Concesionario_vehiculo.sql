e379582ee43f8b193165896ff9e5d202eb5a6514
Dilanpuerta <dylanpy23@gmail.com>
2025-07-08 17:18:31 -0500

    8vo Commit

--- Concesionario_vehiculo.sql
+++ Concesionario_vehiculo.sql
@@ -10,3 +10,19 @@ CREATE TABLE vehiculo (
     precio DECIMAL(10,2) NOT NULL,
     disponible BOOLEAN NOT NULL DEFAULT TRUE
 );
+
+-- Agregamos nuevas funcionalidades a vehiculo luego de la creacion de nuevas tablas--
+ALTER TABLE vehiculo
+ADD COLUMN marca_id INT NOT NULL AFTER id,
+ADD COLUMN tipo_id INT NOT NULL AFTER marca_id;
+
+ALTER TABLE vehiculo
+DROP COLUMN marca;
+
+ALTER TABLE vehiculo
+ADD CONSTRAINT fk_marca
+FOREIGN KEY (marca_id) REFERENCES marca(id);
+
+ALTER TABLE vehiculo
+ADD CONSTRAINT fk_tipo_vehiculo
+FOREIGN KEY (tipo_id) REFERENCES tipo_vehiculo(id);
